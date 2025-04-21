2e89f67f03f62035879b9accf7dfc4c1ce3309be
Bijaya Chhetri <Bijaya Chhetri@Bianca>
2025-04-20 21:19:35 -0400

    update

--- src/test/java/com/selenium/webdriver/Test_13_ToolTip_Text.java
+++ src/test/java/com/selenium/webdriver/Test_13_ToolTip_Text.java
@@ -8,43 +8,32 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-    public class Test_13_ToolTip_Text {
-    	WebDriver driver;
-    	
-    	@BeforeMethod
-    	public void setUp() {
-    		
-    		driver = new ChromeDriver();
-    		driver.manage().window().maximize();
-    	}
-	
-    	@Test
-	public void test_demo() throws InterruptedException {
-		
-		driver.get("https://jqueryui.com/tooltip");
-		driver.switchTo().frame(0);
-		
-		WebElement yourAgeBox = driver.findElement(By.xpath("//input[@id=\"age\"]"));
-		String  yourAgeBoxToolTipText =  yourAgeBox.getDomAttribute("title");
-		System.out.println(yourAgeBoxToolTipText);
-    	}
-		
-		@AfterMethod
-    	public void tearDown() {
-			
-			Thread.sleep(3000);
-			driver.quit();
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+public class Test_13_ToolTip_Text {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() {
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 	}
 
+	@Test
+	public void test_demo() throws InterruptedException {
+
+		driver.get("https://jqueryui.com/tooltip");
+		driver.switchTo().frame(0);
+
+		WebElement yourAgeBox = driver.findElement(By.xpath("//input[@id=\"age\"]"));
+		String yourAgeBoxToolTipText = yourAgeBox.getDomAttribute("title");
+		System.out.println(yourAgeBoxToolTipText);
+	}
+
+	@AfterMethod
+	public void tearDown() throws InterruptedException {
+
+		Thread.sleep(3000);
+		driver.quit();
+	}
 }
+
