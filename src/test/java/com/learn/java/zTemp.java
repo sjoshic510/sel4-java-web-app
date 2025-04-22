0f3f302d3fbcd71352a36b6ca4a849e4c8275f60
Surakshi <sjoshichhetri@gmail.com>
2025-04-22 00:40:39 -0400

    added test-output dir

---
+++ src/test/java/com/learn/java/zTemp.java
@@ -0,0 +1,38 @@
+package com.learn.java;
+
+import org.testng.annotations.Test;
+
+public class zTemp {
+	
+	
+	@Test
+	public void demo() {
+		
+	
+		
+		
+		if(false) {
+			System.out.println("apple");
+			
+		}else if(false) {
+			System.out.println("cat");
+		}else if(false) {
+			System.out.println("ccc");
+		}else if(false) {
+			System.out.println("ddd");
+		}else if(true) {
+			System.out.println("eee");
+		}	
+			else {
+			System.out.println("ball");
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
